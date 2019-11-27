7386fa7ac7c37f679ebfab8271677b7f1f9ea082
Chris Faria <chrisfaria@gmail.com>
2019-11-27 16:29:37 -0500

    renamed app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-proc-launcher-rxjs'`, () => {
+  it(`should have as title 'angular-proc-launcher-ngrx'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-proc-launcher-rxjs');
+    expect(app.title).toEqual('angular-proc-launcher-ngrx');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-proc-launcher-rxjs app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-proc-launcher-ngrx app is running!');
   });
 });
